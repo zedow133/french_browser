459eb5bc4c10fdee48d6ab46b99c6eda380fe83f
Samantha Tissier <stissier@insa-rennes.fr>
2025-05-31 15:46:26 +0200

    embedding single image

--- analysis_keyframes_clip.py
+++ analysis_keyframes_clip.py
@@ -4,21 +4,7 @@ import clip
 import itertools
 from PIL import Image
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
-cos = torch.nn.CosineSimilarity(dim=0)
-
-folder_path = "keyframes_test"
-images = []
-image_files = []
-
-for filename in os.listdir(folder_path):
-    imageFile = os.path.join(folder_path, filename)
-    image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
-    images.append(image)
-    image_files.append(filename)
-
-def text_query_keyframes(text):
+def text_query_keyframes(text, images, image_files, model, device):
     query = clip.tokenize([text]).to(device)
 
     with torch.no_grad():
@@ -41,9 +27,8 @@ def text_query_keyframes(text):
         score = values[i].item()
         print(f"{i+1}. {filename} — Similarité : {score:.4f}")
 
-text_query_keyframes("a planet with black and white stripes")
 
-def image_similarity(imageFile) : 
+def image_similarity(imageFile, images, image_files, model, device, preprocess) : 
     image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
     with torch.no_grad():
         images_tensor = torch.cat(images) # transform la list en Tensor
@@ -64,4 +49,29 @@ def image_similarity(imageFile) :
         score = values[i].item()
         print(f"{i+1}. {filename} — Similarité : {score:.4f}")
     
-image_similarity("keyframes_test/keyframe_shot_003419.jpg")
+def embedding(imageFile, preprocess, device, model) : 
+    image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
+    image_embeddings = model.encode_image(image)
+    print(image_embeddings)
+    return image_embeddings
+
+def main() : 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+
+    folder_path = "keyframes_test"
+    images = []
+    image_files = []
+
+    for filename in os.listdir(folder_path):
+        imageFile = os.path.join(folder_path, filename)
+        image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
+        images.append(image)
+        image_files.append(filename)
+    
+    #text_query_keyframes("a planet with black and white stripes", images, image_files, model, device)
+    #image_similarity("keyframes_test/keyframe_shot_003419.jpg", images, image_files, model, device, preprocess)
+    embedding("keyframes_test/keyframe_shot_003419.jpg", preprocess, device, model)
+
+if __name__ == "__main__":
+    main()
