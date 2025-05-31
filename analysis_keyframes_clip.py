99fff37df0414d332228102cf195b48411bfc432
Samantha Tissier <stissier@insa-rennes.fr>
2025-05-31 15:25:46 +0200

    analysis keyframes

---
+++ analysis_keyframes_clip.py
@@ -0,0 +1,67 @@
+import torch
+import os
+import clip
+import itertools
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+cos = torch.nn.CosineSimilarity(dim=0)
+
+folder_path = "keyframes_test"
+images = []
+image_files = []
+
+for filename in os.listdir(folder_path):
+    imageFile = os.path.join(folder_path, filename)
+    image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
+    images.append(image)
+    image_files.append(filename)
+
+def text_query_keyframes(text):
+    query = clip.tokenize([text]).to(device)
+
+    with torch.no_grad():
+        images_tensor = torch.cat(images) # transform la list en Tensor
+        images_features = model.encode_image(images_tensor)
+        text_features = model.encode_text(query)
+
+    # Normalisation pour similarité cosine
+    images_features /= images_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    # Calcul similarités
+    similarities = (images_features @ text_features.T).squeeze(1)
+    values, indices = similarities.topk(5)
+
+    # Affichage des résultats
+    print(f"\nTop 5 images pour la requête : {text} \n")
+    for i, idx in enumerate(indices):
+        filename = image_files[idx]
+        score = values[i].item()
+        print(f"{i+1}. {filename} — Similarité : {score:.4f}")
+
+text_query_keyframes("a planet with black and white stripes")
+
+def image_similarity(imageFile) : 
+    image = preprocess(Image.open(imageFile)).unsqueeze(0).to(device)
+    with torch.no_grad():
+        images_tensor = torch.cat(images) # transform la list en Tensor
+        images_features = model.encode_image(images_tensor)
+        image_features =  model.encode_image(image)
+
+    images_features /= images_features.norm(dim=-1, keepdim=True)
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+
+    # Calcul similarités
+    similarities = (images_features @ image_features.T).squeeze(1)
+    values, indices = similarities.topk(5)
+
+    # Affichage des résultats
+    print(f"\nTop 5 images pour la requête : {imageFile} \n")
+    for i, idx in enumerate(indices):
+        filename = image_files[idx]
+        score = values[i].item()
+        print(f"{i+1}. {filename} — Similarité : {score:.4f}")
+    
+image_similarity("keyframes_test/keyframe_shot_003419.jpg")
